4f82a1abb78598a00f34cecadee4c996bb86168e
Anderson "Yagasaki" Marlon <anderson18_marlon@hotmail.com>
2019-05-25 18:18:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function changeImage() {
 
 function ptbr() {
     document.getElementById('head1').innerHTML = 'Aplicativos essenciais para seu computador';
-    document.getElementById('head2').innerHTML = 'Softwares para um bom funcionamento e úteis para seu computador | <i>Build 1.6.8</i> de 12 de Abril —';
+    document.getElementById('head2').innerHTML = 'Softwares para um bom funcionamento e úteis para seu computador | <i>Build 1.6.9</i> de 25 de Maio —';
     document.getElementById('backup').innerHTML = 'Programas de Backup / Nuvem';
     document.getElementById('comunication').innerHTML = 'Programas de Comunicações';
     document.getElementById('developer').innerHTML = 'Programas de Desenvolvimento';
@@ -39,11 +39,18 @@ function ptbr() {
     document.getElementById('utylitys').innerHTML = 'Programas de Utilitários';
     document.getElementById('softwareask').innerHTML = 'O software desejado não está na lista? Informe através das redes sociais — <b>Feito com <font color="red">❤</font> pelo Anderson "Yagasaki" Marlon';
     document.getElementById('copyright2').innerHTML = '© Copyright 2017-2019 | Esse site é desenvolvido por <a href="https://www.facebook.com/AndersonMarlonDesign/" target="_blank">Anderson Marlon</a> e hospedado em <a href="https://www.netlify.com/" target="_blank">Netlify</a>. O código do site está hospedado no <a href="http://www.github.com/" target="_blank">GitHub</a>';
+    document.getElementById('dev1').innerHTML = 'Desenvolvedores';
+    document.getElementById('dev2').innerHTML = 'Desenvolvedores';
+    document.getElementById('dev3').innerHTML = 'Desenvolvedores';
+    document.getElementById('dev4').innerHTML = 'Desenvolvedores';
+    document.getElementById('dev5').innerHTML = 'Desenvolvedores';
+    document.getElementById('dev6').innerHTML = 'Desenvolvedores';
+    document.getElementById('dev7').innerHTML = 'Desenvolvedores';
 }
 
 function enusa() {
     document.getElementById('head1').innerHTML = 'Essential applications for your computer';
-    document.getElementById('head2').innerHTML = 'Softwares for a good functioning and useful for your computer | <i> Build 1.6.8 </i> of April 12 -';
+    document.getElementById('head2').innerHTML = 'Softwares for a good functioning and useful for your computer | <i> Build 1.6.9 </i> of May 25th -';
     document.getElementById('backup').innerHTML = 'Backup / Cloud Programs';
     document.getElementById('comunication').innerHTML = 'Communication Programs';
     document.getElementById('developer').innerHTML = 'Development Programs';
@@ -53,4 +60,11 @@ function enusa() {
     document.getElementById('utylitys').innerHTML = 'Utility Programs';
     document.getElementById('softwareask').innerHTML = 'Is the desired software not listed? Report through social networks - <b> Done with <font color = "red"> ❤ </font> by Anderson "Yagasaki" Marlon';
     document.getElementById('copyright2').innerHTML = '© Copyright 2017-2019 | Thie site is build for <a href="https://www.facebook.com/AndersonMarlonDesign/" target="_blank">Anderson Marlon</a> and hosted on <a href="https://www.netlify.com/" target="_blank">Netlify</a>. The source code is hosted on <a href="http://www.github.com/" target="_blank">GitHub</a>.';
+    document.getElementById('dev1').innerHTML = 'Developers';
+    document.getElementById('dev2').innerHTML = 'Developers';
+    document.getElementById('dev3').innerHTML = 'Developers';
+    document.getElementById('dev4').innerHTML = 'Developers';
+    document.getElementById('dev5').innerHTML = 'Developers';
+    document.getElementById('dev6').innerHTML = 'Developers';
+    document.getElementById('dev7').innerHTML = 'Developers';
 }
