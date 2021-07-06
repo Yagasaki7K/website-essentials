28e4b66e6ec88635101b7b3bc05ad38537c65c45
cailir <caiomiranda6@gmail.com>
2021-07-06 16:58:59 -0300

    Update script-loading.js
    Fix the location that loading sends.

--- src/script-loading.js
+++ src/script-loading.js
@@ -1,5 +1,5 @@
 function gotoEssentials() {
 	setInterval(() => {
-			window.location.replace("./public/pages/dashboard.html#backup");
+			window.location.replace("./public/pages/dashboard.html#");
 	}, 5000);
 }
