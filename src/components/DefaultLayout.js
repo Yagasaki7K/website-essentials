188230b3eb14ea6fe457b99a3169b7d61a593243
Anderson Marlon   •   葛城 矢ヶ崎 <anderson18_marlon@hotmail.com>
2021-07-09 14:51:12 -0300

    Fix: v21.07

--- src/components/DefaultLayout.js
+++ src/components/DefaultLayout.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import Header from './header';
 import Leftbar from './Leftbar';
 
 import ContainerDetails from './ContainerDetails'
 import ContentDetails from './ContentDetails';
 
-import { ThemeProvider } from 'styled-components';
-
-import light from '../../styles/theme/light';
-import dark from '../../styles/theme/dark';
-
-const DefaultLayout = (props) => {
+const DefaultLayout = () => {
     return (
         <>
             <ContainerDetails>
--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import React, {useContext} from 'react';
 import Link from 'next/link'
 
