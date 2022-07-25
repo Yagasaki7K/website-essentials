ba61c0669abd91d184f2d07f4834b0b9b4532026
Anderson Marlon <anderson18.marlon@gmail.com>
2022-07-24 21:53:36 -0300

    Fix Image on meta tags #2

--- pages/_document.js
+++ pages/_document.js
@@ -24,13 +24,13 @@ export default class MyDocument extends Document {
                     <meta property="og:url" content="https://webessentials.com.br/"/>
                     <meta property="og:title" content="Web Essentials by Kalify Inc"/>
                     <meta property="og:description" content="Lista de softwares essenciais para ter instalado em seu computador. Especialmente se você acabou de formatar ou não lembra de quais softwares são importantes para você."/>
-                    <meta property="og:image" content="https://i.imgur.com/PZDRcdO.png"/>
+                    <meta property="og:image" content="https://i.imgur.com/Xb2cfxM.png"/>
 
                     <meta property="twitter:card" content="summary_large_image"/>
                     <meta property="twitter:url" content="https://webessentials.com.br/"/>
                     <meta property="twitter:title" content="Web Essentials by Kalify Inc"/>
                     <meta property="twitter:description" content="Lista de softwares essenciais para ter instalado em seu computador. Especialmente se você acabou de formatar ou não lembra de quais softwares são importantes para você."/>
-                    <meta property="twitter:image" content="https://i.imgur.com/PZDRcdO.png"/>
+                    <meta property="twitter:image" content="https://imgur.com/Xb2cfxM.png"/>
                 </Head>
                 <body>
                     <Main />
