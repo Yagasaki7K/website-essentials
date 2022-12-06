4d14748e6a30d4ef6d3d616bf1c54cb41074bcfd
Anderson Marlon <anderson18.marlon@gmail.com>
2022-12-06 19:18:30 -0300

    Added: Poppins and change Release

--- styles/global.js
+++ styles/global.js
@@ -1,13 +1,15 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
     html,
     body {
         padding: 0;
         margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-        background-color: ${props => props.theme.colors.bgcolor}
+        font-family: 'Poppins', sans-serif;
+        background-color: ${(props) => props.theme.colors.bgcolor}
     }
 
     a {
@@ -18,4 +20,4 @@ export default createGlobalStyle`
     * {
     box-sizing: border-box;
     }
-`
+`;
