a799fac39d35eb525310000e092b5db544ad869b
Anderson Marlon <anderson18.marlon@gmail.com>
2024-02-15 19:53:17 -0300

    v24.02

--- src/components/MobileWarning.jsx
+++ src/components/MobileWarning.jsx
@@ -3,7 +3,13 @@ import styled from 'styled-components';
 
 const MobileWarning = () => {
     return (
-        <ContainerDetails>Essentials é exclusivo para uso do Desktop!</ContainerDetails>
+        <ContainerDetails>
+            <img src="/Logo.png" width={350} />
+            <p>
+                Desculpe, mas o uso é exclusivo para Desktop!
+            </p>
+
+        </ContainerDetails>
     )
 }
 
@@ -11,9 +17,14 @@ export default MobileWarning
 
 export const ContainerDetails = styled.div`
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     width: 100%;
     height: 100vh;
     color: var(--purple);
+
+    p {
+        margin-top: 1rem;
+    }
 `;
