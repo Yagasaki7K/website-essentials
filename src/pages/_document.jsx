d3b473d20b15f894ab36db57587908f7282362df
Anderson Marlon <anderson18.marlon@gmail.com>
2024-08-26 00:00:18 -0300

    Fix: Title + SEO

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="pt-br">
-                <title>Essentials | Apps, Navegadores e Extensões</title>
+                <title>Essentials | Apps, Browsers and Extensions</title>
                 <Head>
                     {/* PWA primary color */}
                     <link rel="shortcut icon" type="image/png" href="/favicon.png" />
@@ -21,24 +21,25 @@ export default class MyDocument extends Document {
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
                     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"></link>
 
-                    <meta name="title" content="Essentials | Apps, Navegadores e Extensões" />
+                    <meta name="title" content="Essentials | Apps, Browsers and Extensions" />
                     <meta name="keywords" content="web, essentials, formatting, software, essencial, ethernet,
                     internet, linux, windows, ios, apple, lost, app, desktop, open, source, iobit, dropbox,
-                    google, drive, icloud, mega, onedrive, whatsapp, telegram, hamachi, discord"/>
+                    google, drive, icloud, mega, onedrive, whatsapp, telegram, hamachi, discord, utility, util, apps,
+                    softwares, browsers, extensions"/>
                     <meta name="robots" content="index, follow" />
                     <meta name="author" content="Anderson 'Yagasaki' Marlon" />
 
-                    <meta name="description" content="Lista de softwares essenciais para ter instalado em seu computador. Especialmente se você acabou de formatar ou não lembra de quais softwares são importantes para você." />
+                    <meta name="description" content="List of essential software to have installed on your computer. Especially if you have just formatted or do not remember which software is important to you." />
                     <meta property="og:type" content="website" />
                     <meta property="og:url" content="https://webessentials.com.br/" />
-                    <meta property="og:title" content="Essentials | Apps, Navegadores e Extensões" />
-                    <meta property="og:description" content="Lista de softwares essenciais para ter instalado em seu computador. Especialmente se você acabou de formatar ou não lembra de quais softwares são importantes para você." />
+                    <meta property="og:title" content="Essentials | Apps, Browsers and Extensions" />
+                    <meta property="og:description" content="List of essential software to have installed on your computer. Especially if you have just formatted or do not remember which software is important to you." />
                     <meta property="og:image" content="https://user-images.githubusercontent.com/23272064/234155935-2426d680-0f9d-44b8-af07-45be480aa968.png" />
 
                     <meta property="twitter:card" content="summary_large_image" />
                     <meta property="twitter:url" content="https://webessentials.com.br/" />
-                    <meta property="twitter:title" content="Essentials | Apps, Navegadores e Extensões" />
-                    <meta property="twitter:description" content="Lista de softwares essenciais para ter instalado em seu computador. Especialmente se você acabou de formatar ou não lembra de quais softwares são importantes para você." />
+                    <meta property="twitter:title" content="Essentials | Apps, Browsers and Extensions" />
+                    <meta property="twitter:description" content="List of essential software to have installed on your computer. Especially if you have just formatted or do not remember which software is important to you." />
                     <meta property="twitter:image" content="https://user-images.githubusercontent.com/23272064/234155935-2426d680-0f9d-44b8-af07-45be480aa968.png" />
                 </Head>
                 <body>
